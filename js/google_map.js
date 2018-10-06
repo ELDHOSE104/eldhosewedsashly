b8b4328d4cbda3fe9e3aeb5752708e3b89f8e9e0
eldhose <eldhose104@gmail.com>
2018-10-06 23:30:36 +0530

    images

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(10.062725, 76.619154);
+    var myLatlng = new google.maps.LatLng(10.062725, -76.619154);
     // 39.399872
     // -8.224454
     
